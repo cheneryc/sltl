99065fdb6def7affc075bf7d4ab74d547af69f4c
cheneryc <cheneryc@gmail.com>
2018-04-04 17:41:23 -0700

    Add intrinsic unit tests
    Added two unit tests that cover the sltl::syntax::call_intrinsic_dot
    function

--- sltl_test/src/intrinsic_test.cpp
+++ sltl_test/src/intrinsic_test.cpp
@@ -33,3 +33,79 @@ TEST(intrinsic, dot_product)
 
   ASSERT_EQ(expected, actual);
 }
+
+TEST(intrinsic, call_intrinsic_dot)
+{
+  sltl::syntax::block blk(sltl::syntax::block::global);
+
+  sltl::syntax::block_manager_guard bm;
+  sltl::syntax::block_guard bg(blk);
+
+  sltl::syntax::intrinsic_manager_guard im;
+
+  sltl::vector<float, 3> vec1, vec2;
+  sltl::vector<float, 3>::proxy p1(vec1), p2(vec2);
+
+  auto exp_return = sltl::syntax::call_intrinsic_dot(p1.move(), p2.move());
+
+  ASSERT_TRUE(exp_return);
+  ASSERT_TRUE(dynamic_cast<sltl::syntax::intrinsic_call*>(exp_return.get()));
+}
+
+TEST(intrinsic, call_intrinsic_dot_exceptions)
+{
+  sltl::syntax::block blk(sltl::syntax::block::global);
+
+  sltl::syntax::block_manager_guard bm;
+  sltl::syntax::block_guard bg(blk);
+
+  {
+    sltl::vector<int, 3> vec1;
+    sltl::vector<int, 3>::proxy p1(vec1);
+    sltl::vector<float, 3> vec2;
+    sltl::vector<float, 3>::proxy p2(vec2);
+
+    // call_intrinsic_dot - only floating point types allowed for exp_x argument
+    EXPECT_THROW(sltl::syntax::call_intrinsic_dot(p1.move(), p2.move()), std::exception);
+  }
+
+  {
+    sltl::vector<float, 3> vec1;
+    sltl::vector<float, 3>::proxy p1(vec1);
+    sltl::vector<int, 3> vec2;
+    sltl::vector<int, 3>::proxy p2(vec2);
+
+    // call_intrinsic_dot - only floating point types allowed for exp_y argument
+    EXPECT_THROW(sltl::syntax::call_intrinsic_dot(p1.move(), p2.move()), std::exception);
+  }
+
+  {
+    sltl::matrix<float, 3> vec1;
+    sltl::matrix<float, 3>::proxy p1(vec1);
+    sltl::vector<float, 3> vec2;
+    sltl::vector<float, 3>::proxy p2(vec2);
+
+    // call_intrinsic_dot - only scalar or vector types allowed for exp_x argument
+    EXPECT_THROW(sltl::syntax::call_intrinsic_dot(p1.move(), p2.move()), std::exception);
+  }
+
+  {
+    sltl::vector<float, 3> vec1;
+    sltl::vector<float, 3>::proxy p1(vec1);
+    sltl::matrix<float, 3> vec2;
+    sltl::matrix<float, 3>::proxy p2(vec2);
+
+    // call_intrinsic_dot - only scalar or vector types allowed for exp_y argument
+    EXPECT_THROW(sltl::syntax::call_intrinsic_dot(p1.move(), p2.move()), std::exception);
+  }
+
+  {
+    sltl::vector<float, 3> vec1;
+    sltl::vector<float, 3>::proxy p1(vec1);
+    sltl::vector<float, 4> vec2;
+    sltl::vector<float, 4>::proxy p2(vec2);
+
+    // call_intrinsic_dot - type dimensions must match
+    EXPECT_THROW(sltl::syntax::call_intrinsic_dot(p1.move(), p2.move()), std::exception);
+  }
+}
